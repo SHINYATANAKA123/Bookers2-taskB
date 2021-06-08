36b5cf30edd45c6925db120c62189dbca37e8072
EC2 Default User <ec2-user@ip-172-31-53-238.ec2.internal>
2021-06-08 23:31:04 +0000

    [Add] 検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    get "search", to: "users#search"
+  end
 end
